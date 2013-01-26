85a8ccd682e128411a8e891813fdcab133c49caa
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2013-01-27 02:04:29 +0400

    VKMessage fields now private, getters added

--- Android-VK-Api/src/com/artemzin/android/vk/api/elements/VKMessage.java
+++ Android-VK-Api/src/com/artemzin/android/vk/api/elements/VKMessage.java
@@ -7,7 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- * Class for vk message
+ * Class for vk message <br/>
  * ATTENTION: By default all fields are null
  * @author Artem Zinnatullin
  * @see <a href="http://vk.com/pages?oid=-1&p=%D0%A4%D0%BE%D1%80%D0%BC%D0%B0%D1%82_%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D1%8F_%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D1%85_%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B9">
@@ -15,79 +15,135 @@ import org.json.JSONObject;
  */
 public class VKMessage {
 	
-	/**
-	 * Message mId
-	 */
-	public Long mId;
-	
-	/**
-	 * uId of user who sent message
-	 */
-	public Long uId;
-	
-	/**
-	 * Message date
-	 */
-	public Long date;
+	private Long mId;
+
+    /**
+     * Message mId
+     */
+    public Long getMId() {
+        return mId;
+    }
+
+    private Long uId;
+
+    /**
+     * uId of user who sent message
+     */
+    public Long getUId() {
+        return uId;
+    }
+
+    private Long date;
+
+    /**
+     * Message date
+     */
+    public Long getDate() {
+        return date;
+    }
+
+    private Boolean readState;
 
     /**
      * Read state
      */
-    public Boolean readState;
+    public Boolean getReadState() {
+        return readState;
+    }
+
+    private Boolean out;
 
     /**
      * Is message out (to send)
      */
-    public Boolean out;
+    public Boolean getOut() {
+        return out;
+    }
 
-	/**
-	 * Message title
-	 */
-	public String title;
-	
-	/**
-	 * Message body
-	 */
-	public String body;
-	
-	// TODO Add Attachments
+	private String title;
+
+    /**
+     * Message title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    private String body;
+
+    /**
+     * Message body
+     */
+    public String getBody() {
+        return body;
+    }
+
+    // TODO Add Attachments
+
+    private ArrayList<VKMessage> fwdMessages;
 
     /**
      * ArrayList of forwarded messages
      */
-    public ArrayList<VKMessage> fwdMessages;
+    public ArrayList<VKMessage> getFwdMessages() {
+        return fwdMessages;
+    }
+
+    private Long chatId;
 
     /**
-	 * Only for group dialogs, chat id
-	 */
-	public Long chatId;
-	
-	/**
-	 * Only for group dialogs, ArrayList of last 6 uIds in chat
-	 */
-	public ArrayList<Long> chatActive;
+     * Only for group dialogs, chat id
+     */
+    public Long getChatId() {
+        return chatId;
+    }
+
+    private ArrayList<Long> chatActive;
+
+    /**
+     * Only for group dialogs, ArrayList of last 6 uIds in chat
+     */
+    public ArrayList<Long> getChatActive() {
+        return chatActive;
+    }
+
+    private Integer usersCount;
 
     /**
      * Only for group dialogs, count of users in chat
      */
-    public Integer usersCount;
+    public Integer getUsersCount() {
+        return usersCount;
+    }
 
-	/**
-	 * Only for group dialogs, chat admin`s uId
-	 */
-	public Long adminId;
-	
-	/**
-	 * Is message was deleted by user
-	 */
-	public Boolean deleted;
+    private Long adminId;
+
+    /**
+     * Only for group dialogs, chat admin`s uId
+     */
+    public Long getAdminId() {
+        return adminId;
+    }
+
+    private Boolean deleted;
+
+    /**
+     * Is message was deleted by user
+     */
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    private Boolean emoji;
 
     /**
      * Is message contains emoji smiles
      */
-    public Boolean emoji;
+    public Boolean getEmoji() {
+        return emoji;
+    }
 
-	/**
+    /**
 	 * Creating VKMessage object from JSON
 	 * @param json with message data
 	 */
